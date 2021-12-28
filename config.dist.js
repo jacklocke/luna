13c11223d8c655966a9842b0b15a298a6b45b274
Jack <ashtj14@gmail.com>
2021-12-29 00:00:47 +0100

    multiple search

--- config.dist.js
+++ config.dist.js
@@ -1,6 +1,83 @@
 config = '{\
-    "lang" : "ENG"\
+    "lang" : "ENG",\
+    "search" : "DuckDuckGo"\
 }';
+// Google or DuckDuckGo
+
+links = '{\
+    "A" : {\
+            "nextcloud" : "https://nx18511.your-storageshare.de/",\
+            "reddit" : "https://www.reddit.com/",\
+            "netflix" : "https://www.netflix.com/",\
+            "fosstodon" : "https://fosstodon.org/"\
+        },\
+    "B" : {\
+            "anilist" : "https://anilist.co/home",\
+            "plex" : "https://app.plex.tv/desktop/#!/"\
+        },\
+    "C" : {\
+            "/sci/" : "https://boards.4channel.org/sci/",\
+            "/fit/" : "https://boards.4channel.org/fit/",\
+            "/lit/" : "https://boards.4channel.org/lit/"\
+        },\
+    "D" : {\
+            "protonmail" : "https://mail.protonmail.com/u/0/inbox",\
+            "storygraph" : "https://app.thestorygraph.com/",\
+            "amazon" : "https://www.amazon.co.uk/"\
+        }\
+    }';
+
+// Language: ENGLISH
+ENG = '{ "days" : {\
+    "1" : "Sunday",\
+    "2" : "Monday",\
+    "3" : "Tuesday",\
+    "4" : "Wednesday",\
+    "5" : "Thursday",\
+    "6" : "Friday",\
+    "7" : "Saturday"\
+}, "months" : {\
+    "1" : "January",\
+    "2" : "February",\
+    "3" : "March",\
+    "4" : "April",\
+    "5" : "May",\
+    "6" : "June",\
+    "7" : "July",\
+    "8" : "August",\
+    "9" : "September",\
+    "10" : "October",\
+    "11" : "November",\
+    "12" : "December"\
+}}';
+
+// Language: ITALIAN
+ITA = '{ "days" : {\
+    "1" : "Domenica",\
+    "2" : "Lunedi",\
+    "3" : "Martedi",\
+    "4" : "Mercoledi",\
+    "5" : "Giovedi",\
+    "6" : "Venerdi",\
+    "7" : "Sabato"\
+}, "months" : {\
+    "1" : "Gennaio",\
+    "2" : "Febbraio",\
+    "3" : "Marzo",\
+    "4" : "Aprile",\
+    "5" : "Maggio",\
+    "6" : "Giugno",\
+    "7" : "Luglio",\
+    "8" : "Agosto",\
+    "9" : "Settembre",\
+    "10" : "Ottobre",\
+    "11" : "Novembre",\
+    "12" : "Dicembre"\
+}}';config = '{\
+    "lang" : "ENG",\
+    "search" : "DuckDuckGo"\
+}';
+// Google or DuckDuckGo
 
 links = '{\
     "A" : {\
